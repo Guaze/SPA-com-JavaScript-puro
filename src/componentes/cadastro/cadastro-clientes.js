85d2c8e62d518f94a9f2a689ab4046061af0b68c
Guaze <diegoguaze@gmail.com>
2021-02-20 13:17:34 -0300

    Add files via upload

---
+++ src/componentes/cadastro/cadastro-clientes.js
@@ -0,0 +1,24 @@
+import validaCPF from '../valida/validaCPF.js';
+import { cadastrarClientes } from '../../api/cliente.js';
+
+
+const eventoEnvia = (form) => {
+    const formCadastroCliente = document.querySelector("[data-form]")
+
+    formCadastroCliente.addEventListener("submit",
+        event => { 
+            event.preventDefault()
+    
+            const nome = event.target.querySelector("[data-nome]").value
+            const cpf = event.target.querySelector("[data-cpf]").value
+    
+                if(validaCPF(cpf)){
+                    cadastrarClientes(nome, cpf)
+                } else { 
+                    alert('O CPF não é válido')
+                }        
+        }
+    )
+};
+
+export default eventoEnvia;
